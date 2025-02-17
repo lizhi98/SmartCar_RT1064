b9ba5c142403abc69539d1a4367754c1d87f5209
lizhi <78067291+lizhi98@users.noreply.github.com>
2025-02-17 07:49:46 +0000

    完成电机控制模块大体框架

--- Main/project/user/inc/motor.h
+++ Main/project/user/inc/motor.h
@@ -2,13 +2,28 @@
 #define _CODE_MOTOR_H_
 
 #include "zf_driver_pwm.h"
+#include "zf_driver_pit.h"
 
 #define MOTOR_PWM_FREQUENCY 50
 #define MOTOR_PWM_DUTY_MAX  10000
 
+#define SUM_WRONG_MAX       1000 //TODO
+#define MOTOR_PID_PIT       PIT_CH0// TODO 需要根据实际修改
+#define MOTOR_PID_PIT_TIME  20 // TODO
+
 typedef enum _MotorIndex{
     LEFT,   RIGHT,  FRONT,  MOTOR_INDEX_MAX_PLUS_ONE,
 } MotorIndex;
+
+typedef struct _MotorPID
+{
+    const float KP;
+    const float KI;
+    const float KD;
+          float wrong;
+          float last_wrong;
+          float sum_wrong;
+}MotorPID;
 
 typedef struct _Motor
 {
@@ -18,14 +33,19 @@ typedef struct _Motor
           int32                   pwm_duty;
           int32                   current_speed;
           int32                   set_speed;
+          MotorPID                PID;
 } Motor;
 
-void motor_all_init(void){
-    
-}
+// Motor PWM Control
+void motor_all_init(void);
 void motor_set_duty(MotorIndex index, int32 duty);
 void motor_run_with_speed(MotorIndex index, int32 speed);
 void motor_unpower(MotorIndex index);
 void motor_all_stop(void);
+
+// Motor PID Control
+void motor_pid_init(void);
+void motor_pid_calc_apply(MotorIndex index);
+void motor_pid_timer_call(void);
 
 #endif
--- Main/project/user/src/motor.c
+++ Main/project/user/src/motor.c
@@ -1,5 +1,6 @@
 #include "motor.h"
 
+// TODO
 Motor motors[MOTOR_INDEX_MAX_PLUS_ONE] = {
     {.index = LEFT,  .pwm_duty = 0, .current_speed = 0, .set_speed = 0},
     {.index = RIGHT, .pwm_duty = 0, .current_speed = 0, .set_speed = 0},
@@ -16,8 +17,66 @@ void motor_all_init(void){
 
 // 设置电机PWM通道输出占空比
 void motor_set_duty(MotorIndex index, int32 duty){
-    
+    if(abs(duty) > PWM_DUTY_MAX){
+        (duty > 0) ? (duty = PWM_DUTY_MAX) : (duty = -PWM_DUTY_MAX);
+    }
+    if(duty > 0){
+        pwm_set_duty(motors[index].pwm_channel_backward,    0);
+        pwm_set_duty(motors[index].pwm_channel_forward,     duty);
+    }else if(duty < 0){
+        pwm_set_duty(motors[index].pwm_channel_backward,    -duty);
+        pwm_set_duty(motors[index].pwm_channel_forward,     0);
+    }else if(duty == 0){
+        pwm_set_duty(motors[index].pwm_channel_backward,    0);
+        pwm_set_duty(motors[index].pwm_channel_forward,     0);
+    }else{
+        // TODO 
+        // Wrong!!
+    }
+    // 记录占空比
+    motors[index].pwm_duty = duty;
 }
-void motor_run_with_speed(MotorIndex index, int32 speed);
-void motor_unpower(MotorIndex index);
-void motor_all_stop(void);
+void motor_run_with_speed(MotorIndex index, int32 speed){
+    motors[index].set_speed = speed;
+}
+void motor_unpower(MotorIndex index){
+    pwm_set_duty(index,0);
+}
+void motor_all_stop(void){
+    for(MotorIndex index = 0;index < MOTOR_INDEX_MAX_PLUS_ONE;index ++){
+        motors[index].set_speed = 0;
+    }
+}
+
+void motor_pid_init(void){
+    pit_ms_init(MOTOR_PID_PIT,MOTOR_PID_PIT_TIME);
+}
+void motor_pid_calc_apply(MotorIndex index){
+    double out = 0.;
+    Motor * motor = &motors[index];
+    // 比例
+    motor->PID.wrong = motor->set_speed - motor->current_speed;
+    out += (double) (motor->PID.KP * motor->PID.wrong);
+
+    // 积分
+    motor->PID.sum_wrong += motor->PID.wrong;
+    // 积分限幅
+    if(abs(motor->PID.sum_wrong) > SUM_WRONG_MAX){
+        (motor->PID.sum_wrong > 0) ? (motor->PID.sum_wrong = SUM_WRONG_MAX) : (motor->PID.sum_wrong = -SUM_WRONG_MAX);
+    }
+    out += (double) (motor->PID.KI * motor->PID.sum_wrong);
+
+    // 微分
+    out += (double) (motor->PID.KD * (motor->PID.wrong - motor->PID.last_wrong));
+
+    // 记录wrong
+    motor->PID.last_wrong = motor->PID.wrong;
+
+    // 输出
+    motor_set_duty(index, (int32)out);
+}
+void motor_pid_timer_call(void){
+    for(MotorIndex index = 0;index < MOTOR_INDEX_MAX_PLUS_ONE;index ++){
+        motor_pid_calc_apply(index);
+    }
+}
