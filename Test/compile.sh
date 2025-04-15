41d1b75c24ed03ce6f2a9d678d4b5eaf3e2375f6
ForkΨKILLET <fork_killet@outlook.com>
2025-04-15 22:58:44 +0800

    feat: 图像测试脚本

---
+++ Test/compile.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+dir="$(dirname $0)"
+
+compile () {
+	local project_root="$dir/../Main/project"
+	gcc -I $project_root/user/inc -l m -D IMAGE_DEBUG image_test.c $project_root/user/src/mt_image.c -o ./image_test.out
+}
+
+if [ -f ./image_test.out ]; then
+	rm -f ./image_test.out
+fi
+
+if [ "$1" = -q ]; then
+  compile
+  exit $?
+fi
+
+if ! compile; then
+  echo "[COMPILE ERROR]"
+  exit 1
+else
+  echo "[COMPILE OK]"
+fi
